0510ce25a847716904f19a1a3cf2e144deafaa7b
Chatura Ahangama <chaturaahangama@Chaturas-MacBook-Pro.local>
2016-12-15 22:31:39 -0800

    Getting resources
    Pushing the mechanism to get resources from online

---
+++ Parallel.py
@@ -0,0 +1,48 @@
+import tempfile
+import urllib
+import java.lang
+import time
+
+
+
+def getOS():
+	os = ""
+	ver = sys.platform.lower()
+	ver = java.lang.System.getProperty("os.name").lower()
+	if ver.startswith('mac'):
+		os = "mac"
+	if ver.startswith('win'):
+		os = "win"
+	return os
+
+
+def getMedia(mediaType, fileName):
+	suffix = ""
+	if mediaType == "img":
+		suffix = ".png"
+	else:
+		suffix = ".wav"
+
+	if getOS() == "win":
+		#windows
+		tempFilePath = "C:\\Windows\\Temp\\" + fileName + suffix
+	else:
+		#mac/linux
+		tempFilePath = tempfile.gettempdir() + fileName + suffix
+
+	url = "https://github.com/alphabin/CSUMB-ABSTRACTFACTORY/raw/master/Resources/"+ fileName + suffix
+	data = urllib.urlretrieve(url, tempFilePath)
+
+	if mediaType == "img":
+		myMedia = makePicture(tempFilePath)
+	else:
+		myMedia = makeSound(tempFilePath)
+	return myMedia
+ 
+ 
+def gameEngine():
+         introSong=getMedia("wav","intro1")
+         play(introSong)
+         
+         
+         
